77765cb0552ccb7298584e965f093d1bb56d3344
Jaya-surya-hub <jayasuryag01092005@gmail.com>
2026-04-18 00:31:33 +0530

    Remove WorkerProvider from index.js
    Removed WorkerProvider wrapping from the App component.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { WorkerProvider } from './context/WorkerContext'; // <-- Import it here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Now the ENTIRE app is wrapped in the Context */}
-    <WorkerProvider>
-      <App />
-    </WorkerProvider>
+    <App />
   </React.StrictMode>
 );
